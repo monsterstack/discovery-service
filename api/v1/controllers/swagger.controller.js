9fcc2cc3197bcf29f2d9352f17f8d2a0d8146a72
zacharyrote <zachary.rote@gmail.com>
2017-01-12 14:44:33 -0500

    Inject host and basePath befor returning swagger json

--- api/v1/controllers/swagger.controller.js
+++ api/v1/controllers/swagger.controller.js
@@ -1,10 +1,16 @@
 'use strict';
 const appRoot = require('app-root-path');
 const HttpStatus = require('http-status');
+const ip = require('ip');
 
 const swagger = require(appRoot + '/api/swagger/swagger.json');
 const getSwagger = (app) => {
   return function(req, res) {
+    let host = ip.address();
+    //@TODO: Base Path should be in config..
+    let basePath = '/api/v1';
+    swagger.host = host;
+    swagger.basePath = basePath;
     res.status(HttpStatus.OK).send(swagger);
   }
 }

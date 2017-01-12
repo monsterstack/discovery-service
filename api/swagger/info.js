8db6121a5e61ac317f1b29315176365dd130f1e7
zacharyrote <zachary.rote@gmail.com>
2017-01-12 15:58:52 -0500

    Add swagger json

---
+++ api/swagger/info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+   info: {
+     title: "Discovery Service",
+     description: "This service helps you discovery other services",
+     version: "1.0.0"
+   }
+ }
--- libs/serviceLifecycle.js
+++ libs/serviceLifecycle.js
@@ -46,6 +46,7 @@ class ServiceLifecycle {
       type: 'DiscoveryService',
       healthCheckRoute: '/health',
       schemaRoute: '/swagger.json',
+      docsRoute: '/docs/index.html',
       timestamp: new Date(),
       id: id,
       region: announce.region,

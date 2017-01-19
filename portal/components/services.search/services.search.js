19ae146d8a3f92a03998d43e34ea67a29df52b32
Jack Min <jmin@cchs.com>
2017-01-19 16:49:43 -0500

    refresh service types onClick, will add socket.io later

--- portal/components/services.search/services.search.js
+++ portal/components/services.search/services.search.js
@@ -42,6 +42,7 @@
         vm.events.onPageChange = onPageChange;
         vm.events.onItemPerPageChange = onItemPerPageChange;
         vm.events.onDocClick = onDocClick;
+        vm.events.refreshServiceTypes = refreshServiceTypes;
 
         function initialize($http){
             let opt = {
@@ -52,34 +53,9 @@
                 regionFilter: "",
                 statusFilter: ""
             }
-            $http.get("/constants",{
-                headers:{
-                    "Content-Type": "application/json"
-                }
-            }).then(
-                function successCallback(results) {
-                    vm.display.stages = results.data.stageTypes;
-                    vm.display.regions = results.data.regionTypes;
-                },
-                function errorCallback(error) {
-                    console.log("error: ", error);
-                }
-            );
-            $http.get("/api/v1/services/_types",{
-                headers:{
-                    "Content-Type": "application/json"
-                }
-            }).then(
-                function successCallback(results) {
-                    let typeObjArr = results.data;
-                    for (let i=0; i<typeObjArr.length; i++) {
-                        vm.display.serviceTypes.push(typeObjArr[i].type);
-                    }
-                },
-                function errorCallback(error) {
-                    console.log("error: ", error);
-                }
-            );
+            getConstantsForFiltering();
+            getServiceTypes();
+
             search(opt, $http);
         }
 
@@ -149,6 +125,47 @@
 
         function onDocClick(url){
             $window.open("https://www.google.com");
+        }
+
+        function refreshServiceTypes(){
+            getServiceTypes();
+        }
+
+
+
+        function getConstantsForFiltering(){
+            $http.get("/constants",{
+                headers:{
+                    "Content-Type": "application/json"
+                }
+            }).then(
+                function successCallback(results) {
+                    vm.display.stages = results.data.stageTypes;
+                    vm.display.regions = results.data.regionTypes;
+                },
+                function errorCallback(error) {
+                    console.log("error: ", error);
+                }
+            );
+        }
+
+        function getServiceTypes(){
+            $http.get("/api/v1/services/_types",{
+                headers:{
+                    "Content-Type": "application/json"
+                }
+            }).then(
+                function successCallback(results) {
+                    let typeObjArr = results.data;
+                    vm.display.serviceTypes = [];
+                    for (let i=0; i<typeObjArr.length; i++) {
+                        vm.display.serviceTypes.push(typeObjArr[i].type);
+                    }
+                },
+                function errorCallback(error) {
+                    console.log("error: ", error);
+                }
+            );
         }
 
 
